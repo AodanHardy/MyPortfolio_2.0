63459f8431bc9aeca17482657ba838e7d7f1a210
Aodan <hardy-a4@ulster.ac.uk>
2025-10-20 20:47:17 +0100

    Initial commit of my portfolio project

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from core import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
+    path("experience/", views.experience, name="experience"),
+    path("contact/", views.contact, name="contact"),
+    path("projects/", views.projects, name="projects"),
+]
