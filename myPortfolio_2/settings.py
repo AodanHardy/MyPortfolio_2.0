d3f3d286deaa668e8a69cba248a7d09e74ccde6d
Aodan <hardy-a4@ulster.ac.uk>
2025-11-02 12:22:09 +0000

    adding pi to allowed hosts

--- myPortfolio_2/settings.py
+++ myPortfolio_2/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = "django-insecure-71xzdzdzw*yl2_&-!76g(da^dzl%px+_9_n+t20bfll&088@q#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "raspberrypi.local",
+]
 
 
 # Application definition
